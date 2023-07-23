fe746392bb85e69dd8ecb52c05b531364631c8b5
Joao <joaop.carvalho01@gmail.com>
2023-07-23 15:51:42 -0300

    Peer Finalizado (Em Teoria)

--- peer.py
+++ peer.py
@@ -4,117 +4,134 @@ import random
 
 
 class Peer:
+    ## Verificado
     def __init__(self):
         self.endereco_servidores = [] #Armazena as conexões com os servidores
-        self.tabelahash = {}
-        # Inicialize outras variáveis e estruturas necessárias
+        self.tabelahash = {} # Tabela hash
+
 
     # Seção 4.a - Inicialização do Peer
     def inicializar(self):
+        ## Atualizar para 3 Servidores
         num_servidores = 2  # Número de servidores
 
+        # Espera por IP e Porta do num_servidores
         for i in range(num_servidores):
-            while True:
-                try:
-                    ip = input(f"Informe o IP do servidor {i+1}: ")
-                    porta = int(input(f"Informe a porta do servidor {i+1}: "))
-
-                    self.endereco_servidores.append((ip, porta))                    
-
-                    break
-                except ValueError:
-                    print("Por favor, insira um valor válido para a porta.")
-
-    # Seção 4.b - Envio do PUT
-    def enviar_requisicao_put(self, key, value):
-        servidor_escolhido = random.choice(self.endereco_servidores)
-        try:
-            mensagem = Mensagem("PUT", key=key, value=value)
-            mensagem_serializada = mensagem.to_json()
+            ip = input(f"Informe o IP do servidor {i+1}: ")
+            porta = int(input(f"Informe a porta do servidor {i+1}: "))
             
-            # Envia a mensagem serializada através da conexão com o servidor
-            conexao_server = socket.socket()
-            conexao_server.connect(servidor_escolhido)
-            conexao_server.send(mensagem_serializada.encode())
-            
-            # conexao_server.close()
+            # Adiciona na lista endereco_servidores
+            self.endereco_servidores.append((ip, porta))                    
 
-            # Resposta do servidor
-            resposta_serializada = conexao_server.recv(1024).decode()
-
-            # Tratamento da resposta do servidor
-            resposta = Mensagem.from_json(resposta_serializada)
-            if resposta.tipo == "PUT_OK":
-                print(f"PUT_OK key: {key} value: {value} timestamp: {resposta.timestamp} realizada no servidor {endereco_servidores.getpeername()}")
-
-            conexao_server.close()
-
-        except Exception as e:
-            print(f"Erro ao enviar a requisição PUT: {e}")
-        
-    def enviar_requisicao_get(self, key):
-        servidor_escolhido = random.choice(self.endereco_servidores)
-        try:
-            mensagem = Mensagem("GET", key=key)
-            mensagem_serializada = mensagem.to_json()
-
-            # Envia a mensagem serializada para o servidor escolhido
-            conexao_server = socket.socket()
-            conexao_server.connect(servidor_escolhido)
-            conexao_server.send(mensagem_serializada.encode())
-
-            # Resposta do servidor
-            resposta_serializada = conexao_server.recv(1024).decode()
-
-            # Tratamento da resposta do servidor
-            resposta = Mensagem.from_json(resposta_serializada)
-            if resposta.tipo == "GET_RESPONSE":
-                print(f"GET key: {key} value: {resposta.value} obtido do servidor {servidor_escolhido}, meu timestamp {resposta.timestamp} e do servidor {resposta.timestamp_servidor}")
-
-            conexao_server.close()
-
-        except Exception as e:
-            print(f"Erro ao enviar a requisição GET: {e}")
-    
+    # Seção 6 (Peer) - Menu Interativo
     def exibir_menu(self):
+        
+        # Responsável por obrigar a inicialização do Peer
         while True:
+            opcao = input("Aperte 1 para Inicializar o Peer:")
+            if (opcao == "1"):
+                break
+        
+
+        self.inicializar()
+
+        while True:        
             print("\nMenu Interativo")
-            print("1. Inicializar")
-            print("2. PUT")
-            print("3. GET")
-            print("4. Sair")
+            print("1. PUT")
+            print("2. GET")
             
             opcao = input("Escolha uma opção: ")
             
+            # Requisição PUT
             if opcao == "1":
-                self.inicializar()
+                chave = input("Digite a chave: ")
+                valor = input("Digite o valor: ")
+                self.enviar_requisicao_put(chave, valor)
+            
+            # Requisição GET
             elif opcao == "2":
-                key = input("Digite a chave: ")
-                value = input("Digite o valor: ")
-                self.enviar_requisicao_put(key, value)
-            elif opcao == "3":
-                key = input("Digite a chave: ")
-                self.enviar_requisicao_get(key)
-            elif opcao == "4":
+                chave = input("Digite a chave: ")
+                self.enviar_requisicao_get(chave)
+            
+            # Encerra Programa
+            elif opcao == "0":
                 break
+            
+            # Número fora das possibilidades
             else:
                 print("Opção inválida. Por favor, escolha uma opção válida.")
+        
+    # Seção 4.b - Envio do PUT
+    def enviar_requisicao_put(self, chave, valor):
+        
+        # Cria e serializa Mensagem GET que será enviada    
+        mensagem = Mensagem('PUT', (chave, valor))
+        mensagem_serializada = mensagem.to_json().encode()
+        
+        # Escolher Servidor aleatoriamente (dentro da lista de endereco_servidores)
+        servidor_escolhido = random.choice(self.endereco_servidores)
+        
+        # Envia a mensagem serializada através da conexão com o servidor
+        conexao_server = socket.socket()
+        conexao_server.connect(servidor_escolhido)
+        conexao_server.send(mensagem_serializada)
+        
+        # Resposta do servidor e desserialização dela
+        resposta_serializada = conexao_server.recv(1024).decode()
+        resposta = Mensagem.from_json(resposta_serializada)
+        
+        # Fechando conexão com o Servidor
+        conexao_server.close()
 
-    
-    def executar(self):
-        # Implemente a lógica principal do peer
-        self.exibir_menu()
-    
+        # Tratamento da Resposta PUT
+        if resposta.tipo == "PUT_OK":
+            self.tabelahash.update({chave:( valor, resposta.conteudo)})
+
+            # Seção 6 (Peer) - Print PUT_OK
+            print(f"PUT_OK key: {chave} value: {valor} timestamp: {resposta.conteudo}")
+
+ 
+    # Seção 4.c - Envio da Requisicação GET     
+    def enviar_requisicao_get(self, chave):
+        
+        # Verifica se a chave já existe na tabela Hash e pega a timestamp associada, ou cria ela.
+        if chave in self.tabelahash:
+            timestamp = self.tabelahash[chave][1]
+        else:
+            timestamp = 0
+        
+        # Cria e serializa Mensagem GET que será enviada    
+        mensagem = Mensagem("GET", (chave, timestamp))
+        mensagem_serializada = mensagem.to_json().encode()
+
+
+        # Escolher Servidor aleatoriamente (dentro da lista de endereco_servidores)
+        servidor_escolhido = random.choice(self.endereco_servidores)
+        
+        # Envia a mensagem serializada para o servidor escolhido
+        conexao_server = socket.socket()
+        conexao_server.connect(servidor_escolhido)
+        conexao_server.send(mensagem_serializada)
+
+        # Resposta do servidor e desserialização dela
+        resposta_serializada = conexao_server.recv(1024).decode()
+        resposta = Mensagem.from_json(resposta_serializada)
+        
+        # Fechando conexão com o Servidor
+        conexao_server.close()
+        
+        # Tratamento da Resposta GET
+        if resposta.tipo == 'GET_OK':
+            self.tabelahash.update({chave:resposta.conteudo})
+            print(f'{chave}:{resposta.conteudo}')
+        elif resposta.tipo == 'ERRO':
+            print('TRY_OTHER_SERVER_OR_LATER')
+        else:
+            print('Erro Inesperado. A resposta obtida foi: ', resposta)
+            
+ 
 # Seção para inicialização do peer
 if __name__ == "__main__":
     peer = Peer()
-    peer_ip = input("Peer IP: ")
-    peer_porta = int(input("Peer Porta: "))
-    
-    # Configurar as informações do peer
-    peer.ip_servidores = [peer_ip]  # Insira os IPs dos servidores
-    peer.portas_servidores = [peer_porta]  # Insira as portas dos servidores
-    # Outras configurações do peer, se necessário
-    
-    peer.executar()
+    peer.exibir_menu()
 
